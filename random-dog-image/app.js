6edbf2d4d82b010b13e3dad3cb8b10dcf4bd6973
Chetan Dhapola <chetandhapola44@gmail.com>
2026-03-19 22:27:39 +0530

    Implement error handling for image fetch
    Added error handling for image fetching.

--- random-dog-image/app.js
+++ random-dog-image/app.js
@@ -5,8 +5,15 @@ let image = document.querySelector("#image");
 let linkTag = document.querySelector("#link");
 
 button.addEventListener("click", async () => {
-    let response = await axios.get(url);
-    let imgUrl = response.data.message;
-    image.src = imgUrl;         
-    linkTag.href = imgUrl;       
+    try {
+        let response = await axios.get(url);
+        let imgUrl = response.data.message;
+
+        image.src = imgUrl;
+        linkTag.href = imgUrl;
+
+    } catch (error) {
+        console.log("Error fetching image:", error);
+        alert("Failed to load image ❌");
+    }
 });
